e02c8d326d7834e794c9e65d6be6744c9b771307
jk-lyh <932290707@qq.com>
2016-11-02 22:59:12 +0800

    todo

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 $(function(){
-	var input=$(".top input");
-	var add=$(".tianjia");
-//	var add2=$(".tianjia2");
-//	var add3=$(".tianjia3");
-//	var add4=$(".tianjia4");
+	var input1=$(".top1 input");
+	var input2=$(".top2 input");
+	var input3=$(".top3 input");
+	var input4=$(".top4 input");
+	var add1=$(".tianjia1");
+	var add2=$(".tianjia2");
+	var add3=$(".tianjia3");
+	var add4=$(".tianjia4");
 	var ul=$(".shijian");
-	var v=$.trim(input.val());
 	var todos=[];
 	
 	var d = new Date(new Date().getTime());
@@ -23,58 +25,58 @@ $(function(){
 			$("<li class="+c+"><div class='con'>"+todos[i].name+"</div><div class='son'>删除</div></li>").appendTo('ul');
 		}
 	}
-	add.on('touchend',function(){
-		v=$.trim(input.val());
-		var todo={
-			name:v,
+	$('.tianjia1').on('touchend',function(){
+		var v1=$.trim(input1.val());
+		var todo1={
+			name:v1,
 			state:0
 		};
-		input.val('');
-		todos.push(todo);
+		input1.val('');
+		todos.push(todo1);
 		localStorage.todos=JSON.stringify(todos);
-		var c=(todo.state)?'don':'ton';
-		$('<li class='+c+'><div class="con">'+todo.name+'</div><div class="son">删除</div></li>').appendTo('ul');
+		var c=(todo1.state)?'don':'ton';
+		$('<li class='+c+'><div class="con">'+todo1.name+'</div><div class="son">删除</div></li>').appendTo('ul');
 		$('.top').css('display','none')
 	});
-//	add2.on('touchend',function(){
-//		v=$.trim(input.val());
-//		var todo={
-//			name:v,
-//			state:0
-//		};
-//		input.val('');
-//		todos.push(todo);
-//		localStorage.todos=JSON.stringify(todos);
-//		var c=(todo.state)?'don':'ton';
-//		$('<li class='+c+2+'><div class="con">'+todo.name+'</div><div class="son">删除</div></li>').appendTo('ul');
-//		$('.top').css('display','none')
-//	});
-//	add3.on('touchend',function(){
-//		v=$.trim(input.val());
-//		var todo={
-//			name:v,
-//			state:0
-//		};
-//		input.val('');
-//		todos.push(todo);
-//		localStorage.todos=JSON.stringify(todos);
-//		var c=(todo.state)?'don':'ton';
-//		$('<li class='+c+3+'><div class="con">'+todo.name+'</div><div class="son">删除</div></li>').appendTo('ul');
-//		$('.top').css('display','none')
-//	});
-//	add4.on('touchend',function(){
-//		v=$.trim(input.val());
-//		var todo={
-//			name:v,
-//			state:0
-//		};
-//		input.val('');
-//		todos.push(todo);
-//		localStorage.todos=JSON.stringify(todos);
-//		var c=(todo.state)?'don':'ton';
-//		$('<li class='+c+4+'><div class="con">'+todo.name+'</div><div class="son">删除</div></li>').appendTo('ul');
-//		$('.top').css('display','none')
-//	})
+	add2.on('touchend',function(){
+		var v2=$.trim(input2.val());
+		var todo2={
+			name:v2,
+			state:0
+		};
+		input2.val('');
+		todos.push(todo2);
+		localStorage.todos=JSON.stringify(todos);
+		var c=(todo2.state)?'don':'ton';
+		$('<li class='+c+'><div class="con">'+todo2.name+'</div><div class="son">删除</div></li>').appendTo('ul');
+		$('.top').css('display','none')
+	});
+	add3.on('touchend',function(){
+		var v3=$.trim(input3.val());
+		var todo3={
+			name:v3,
+			state:0
+		};
+		input3.val('');
+		todos.push(todo3);
+		localStorage.todos=JSON.stringify(todos);
+		var c=(todo3.state)?'don':'ton';
+		$('<li class='+c+'><div class="con">'+todo3.name+'</div><div class="son">删除</div></li>').appendTo('ul');
+		$('.top').css('display','none')
+	});
+	add4.on('touchend',function(){
+		var v4=$.trim(input4.val());
+		var todo4={
+			name:v4,
+			state:0
+		};
+		input4.val('');
+		todos.push(todo4);
+		localStorage.todos=JSON.stringify(todos);
+		var c=(todo4.state)?'don':'ton';
+		$('<li class='+c+'><div class="con">'+todo4.name+'</div><div class="son">删除</div></li>').appendTo('ul');
+		$('.top').css('display','none')
+	})
 	
 	var starpos;
 	$('ul').on('touchstart','li',function(e){
@@ -157,27 +159,32 @@ $(function(){
 		$('.xuanze').css('display','none');
 	});
 	
-	$('.xuanze').on('touchend','div',function(){
-		$('.top').css('display','block')
+	$('.xuanze1').on('touchend',function(){
+		$('.top1').css('display','block')
 		$('.xuanze').css('display','none')
 	})
-//	$('.xuanze2').on('touchend',function(){
-//		$('.top2').css('display','block')
-//		$('.xuanze').css('display','none')
-//	})
-//	$('.xuanze3').on('touchend',function(){
-//		$('.top3').css('display','block')
-//		$('.xuanze').css('display','none')
-//	})
-//	$('.xuanze4').on('touchend',function(){
-//		$('.top4').css('display','block')
-//		$('.xuanze').css('display','none')
-//	})
+	$('.xuanze2').on('touchend',function(){
+		$('.top2').css('display','block')
+		$('.xuanze').css('display','none')
+	})
+	$('.xuanze3').on('touchend',function(){
+		$('.top3').css('display','block')
+		$('.xuanze').css('display','none')
+	})
+	$('.xuanze4').on('touchend',function(){
+		$('.top4').css('display','block')
+		$('.xuanze').css('display','none')
+	})
 	$('.qx').on('touchend',function(){
 		$('.top').css('display','none')
 	})
 	
-	
+	$('.cd').on('touchend',function(){
+		$('.mbx').css('display','block')
+	});
+	$('.fh').on('touchend',function(){
+		$('.mbx').css('display','none')
+	})
 	
 	
 	
